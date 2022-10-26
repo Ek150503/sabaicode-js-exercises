c3933858ffdca19ca19a64503f5ed9ffa684b2af
Ek E <Ekzin15@gmail.com>
2022-10-26 13:45:01 +0700

    Update 04-exercise

--- 04-exercise.js
+++ 04-exercise.js
@@ -1,6 +1,8 @@
 const check = (arr,item) =>{
-    if(arr.includes(item)) return true;
-    else return false;
+    // if(arr.includes(item)) return true;
+    // else return false;
+
+    return arr.includes(item);
 }
 
 console.log(check([1,2,3,4,5,6], 6));
