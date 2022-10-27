164717840b9481ff9cabb3b46ada4eaaf1591407
Ek E <Ekzin15@gmail.com>
2022-10-27 09:50:46 +0700

    09-exercise NEW-VERSION

--- 09-exercise.js
+++ 09-exercise.js
@@ -1,13 +1,9 @@
 const numberSplit = (num) => {
     let num1;
     let num2;
-    if(num % 2 === 0){
-        num1 = num2 = num / 2;
-    }
-    else{
-        num1 = Math.floor(num / 2);
-        num2 = Math.floor(num / 2) + 1;
-    }
+    
+    num1 = Math.floor(num / 2)
+    num2 = Math.ceil(num / 2)
 
     return [num1, num2]
 }
@@ -16,3 +12,5 @@ console.log(numberSplit(4))
 console.log(numberSplit(10))
 console.log(numberSplit(11))
 console.log(numberSplit(-9))
+
+
